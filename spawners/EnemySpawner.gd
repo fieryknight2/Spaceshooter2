1dee9424ada2423018d188bfa2a4f2ed3e0790aa
fieryknight2 <84808424+fieryknight2@users.noreply.github.com>
2022-07-14 15:04:21 -0500

    Changed boss to starting by time instead of score

--- spawners/EnemySpawner.gd
+++ spawners/EnemySpawner.gd
@@ -3,27 +3,17 @@ extends Node2D
 export (PackedScene) var enemy
 export (PackedScene) var enemy2
 export (PackedScene) var boss1
-export (int) var boss1_score
+export (int) var boss1_time
 export (int) var increase_score_by
 
 var prev1_score = 0
 func _ready():
 	$E1.start(15 / get_tree().current_scene.speed_scale)
 	$E2.start(rand_range(35, 50) / get_tree().current_scene.speed_scale)
+	$Boss.start(rand_range(boss1_time - 15, boss1_time + 15) / get_tree().current_scene.speed_scale)
 
 func _process(_delta):
-	if get_tree().current_scene.score - prev1_score >= boss1_score:
-		prev1_score = get_tree().current_scene.score
-		boss1_score += increase_score_by
-		
-		var e = boss1.instance()
-		
-		e.position.x = get_viewport().size.x / 2
-		e.position.y = -100
-		
-		get_tree().current_scene.add_child(e)
-		
-	elif get_tree().current_scene.score - prev1_score >= boss1_score - 1500:
+	if $Boss.time_left < 5:
 		$E1.stop()
 		$E2.stop()
 		
@@ -46,3 +36,13 @@ func _on_E2_timeout():
 	
 	get_tree().current_scene.call_deferred("add_child", e)
 	$E2.start(rand_range(35, 50) / get_tree().current_scene.speed_scale)
+
+
+func _on_Boss_timeout():
+	var e = boss1.instance()
+	
+	e.position.x = get_viewport().size.x / 2
+	e.position.y = -100
+	
+	get_tree().current_scene.add_child(e)
+	$Boss.start(rand_range(boss1_time - 15, boss1_time + 15) / get_tree().current_scene.speed_scale)
