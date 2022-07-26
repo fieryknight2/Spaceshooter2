315f2b5a6ea4f2a6c408f5ade9a80bc7522ecacf
Fieryknight2 <84808424+fieryknight2@users.noreply.github.com>
2022-07-26 10:50:28 -0600

    Added color and rotation variation for stars

--- assets/Star.gd
+++ assets/Star.gd
@@ -4,15 +4,28 @@ export (float) var move_speed
 export (float) var max_scale
 export (float) var min_scale
 
+export (Color) var max_yellow
+export (Color) var max_green
+export (Color) var base_color
+
 var ms
+var rand = RandomNumberGenerator.new()
 
 func _ready():
+	rand.randomize()
+	rotation_degrees = rand.randi_range(0, 360)
 	
 	# set to a random speed
-	scale.x = rand_range(min_scale, max_scale)
+	scale.x = rand.randf_range(min_scale, max_scale)
 	scale.y = scale.x
 	# set speed so the smaller the scale the slower you go
-	ms = rand_range(move_speed, move_speed*move_speed*scale.x)
+	ms = rand.randf_range(move_speed, move_speed*move_speed*scale.x)
+	
+	var rand_yellow = base_color.linear_interpolate(max_yellow, rand.randf_range(0.0, 1.0))
+	var rand_green = base_color.linear_interpolate(max_green, rand.randf_range(0.0, 1.0))
+	modulate = rand_green.blend(rand_yellow)
+	
+	
 	
 func _process(delta):
 	# move star down
